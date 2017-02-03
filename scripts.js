da0917ca43b82494905ebd218a9e790334c45b8e
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2017-02-03 14:12:40 +0530

    bhai ho gaya

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var a;
+
 function callme() {
     document.body.style.backgroundColor = "pink";
+    document.body.style.color = "red";
+    document.body.innerHTML = window.innerWidth;
 }
 
 document.addEventListener('click', callme);
